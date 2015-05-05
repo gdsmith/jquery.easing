4efaba2499971a60d5275facd714678ee16e8d1f
George McGinley Smith <george@mukuru.com>
2015-05-05 08:46:49 +0200

    isolate jQuery use and remove Licence to it's own file

--- jquery.easing.compatibility.js
+++ jquery.easing.compatibility.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Easing Compatibility v1 - http://gsgd.co.uk/sandbox/jquery.easing.php
+ * Easing Compatibility v1 - http://gsgd.co.uk/sandbox/jquery/easing
  *
  * Adds compatibility for applications that use the pre 1.2 easing names
  *
@@ -8,51 +8,52 @@
  *   http://www.opensource.org/licenses/mit-license.php
  */
 
-jQuery.extend( jQuery.easing,
+(function($){
+$.extend( $.easing,
 {
 	easeIn: function (x, t, b, c, d) {
-		return jQuery.easing.easeInQuad(x, t, b, c, d);
+		return $.easing.easeInQuad(x, t, b, c, d);
 	},
 	easeOut: function (x, t, b, c, d) {
-		return jQuery.easing.easeOutQuad(x, t, b, c, d);
+		return $.easing.easeOutQuad(x, t, b, c, d);
 	},
 	easeInOut: function (x, t, b, c, d) {
-		return jQuery.easing.easeInOutQuad(x, t, b, c, d);
+		return $.easing.easeInOutQuad(x, t, b, c, d);
 	},
 	expoin: function(x, t, b, c, d) {
-		return jQuery.easing.easeInExpo(x, t, b, c, d);
+		return $.easing.easeInExpo(x, t, b, c, d);
 	},
 	expoout: function(x, t, b, c, d) {
-		return jQuery.easing.easeOutExpo(x, t, b, c, d);
+		return $.easing.easeOutExpo(x, t, b, c, d);
 	},
 	expoinout: function(x, t, b, c, d) {
-		return jQuery.easing.easeInOutExpo(x, t, b, c, d);
+		return $.easing.easeInOutExpo(x, t, b, c, d);
 	},
 	bouncein: function(x, t, b, c, d) {
-		return jQuery.easing.easeInBounce(x, t, b, c, d);
+		return $.easing.easeInBounce(x, t, b, c, d);
 	},
 	bounceout: function(x, t, b, c, d) {
-		return jQuery.easing.easeOutBounce(x, t, b, c, d);
+		return $.easing.easeOutBounce(x, t, b, c, d);
 	},
 	bounceinout: function(x, t, b, c, d) {
-		return jQuery.easing.easeInOutBounce(x, t, b, c, d);
+		return $.easing.easeInOutBounce(x, t, b, c, d);
 	},
 	elasin: function(x, t, b, c, d) {
-		return jQuery.easing.easeInElastic(x, t, b, c, d);
+		return $.easing.easeInElastic(x, t, b, c, d);
 	},
 	elasout: function(x, t, b, c, d) {
-		return jQuery.easing.easeOutElastic(x, t, b, c, d);
+		return $.easing.easeOutElastic(x, t, b, c, d);
 	},
 	elasinout: function(x, t, b, c, d) {
-		return jQuery.easing.easeInOutElastic(x, t, b, c, d);
+		return $.easing.easeInOutElastic(x, t, b, c, d);
 	},
 	backin: function(x, t, b, c, d) {
-		return jQuery.easing.easeInBack(x, t, b, c, d);
+		return $.easing.easeInBack(x, t, b, c, d);
 	},
 	backout: function(x, t, b, c, d) {
-		return jQuery.easing.easeOutBack(x, t, b, c, d);
+		return $.easing.easeOutBack(x, t, b, c, d);
 	},
 	backinout: function(x, t, b, c, d) {
-		return jQuery.easing.easeInOutBack(x, t, b, c, d);
+		return $.easing.easeInOutBack(x, t, b, c, d);
 	}
-});
+});})(jQuery);
