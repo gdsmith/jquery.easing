4832b3e6a8d34da7236638b7ea01f97e44118aff
dwebbuilder <9950090+dwebbuilder@users.noreply.github.com>
2017-10-09 19:44:48 +0200

    Version updated to 1.4.1

--- jquery.easing.js
+++ jquery.easing.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Easing v1.4.0 - http://gsgd.co.uk/sandbox/jquery/easing/
+ * jQuery Easing v1.4.1 - http://gsgd.co.uk/sandbox/jquery/easing/
  * Open source under the BSD License.
  * Copyright © 2008 George McGinley Smith
  * All rights reserved.
